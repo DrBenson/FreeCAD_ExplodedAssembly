bba3dccb564dd50d93bf08ca8baff376a6b7d595
DrBenson <Benson.Dr@GMail.com>
2024-07-11 10:13:33 +0800

    Update Tranditional Chinese translation.

---
+++ translations/ExplodedAssembly.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ExplodedAssembly</name>
+    <message>
+        <location filename="../CameraAnimation.py" line="63"/>
+        <source>Enable Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="64"/>
+        <location filename="../CameraAnimation.py" line="65"/>
+        <source>Interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="66"/>
+        <location filename="../CameraAnimation.py" line="67"/>
+        <location filename="../CameraAnimation.py" line="68"/>
+        <location filename="../CameraAnimation.py" line="69"/>
+        <source>Camera Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="70"/>
+        <source>Camera Transition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="100"/>
+        <source>
+Manual camera created
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="44"/>
+        <source>Create Bolt Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="47"/>
+        <source>Create a special exploded group for screws, nuts, bolts... 
+ Select circular edges of the shape you want to animate, then
+ select one face (arbitrary shape) wich has as normal vector
+in the direction in wich you want to move the selected shapes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="63"/>
+        <source>Create Simple Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="66"/>
+        <source>Select the objects you want to explode and
+finally the face which its normal is the trajectory director vector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="81"/>
+        <source>Create Route</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="84"/>
+        <source>Select first one or more shapes and last the wire or sketch that represents the trajectory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="101"/>
+        <source>PlaceBefore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="104"/>
+        <source>Select the trajectories you want to reallocate (in order) and finally
+the trajectory before which you want to place them</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="126"/>
+        <source>
+ Select exploded assembly trajectory objects only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="132"/>
+        <source>Modify Individual Object Trajectory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="135"/>
+        <source>Explode objects on several directions at once
+Select the trajectory, select the object and
+the face which its normal is the new director
+vector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="156"/>
+        <location filename="../EAInit.py" line="174"/>
+        <location filename="../EAInit.py" line="192"/>
+        <source>Manual Animation Camera</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="159"/>
+        <location filename="../EAInit.py" line="177"/>
+        <location filename="../EAInit.py" line="195"/>
+        <source>Create a transition between two cameras</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="210"/>
+        <source>Play Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="213"/>
+        <source>Run the assembly animation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="237"/>
+        <source>Play Backwards</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="266"/>
+        <source>StopAnimation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="269"/>
+        <source>Stops the animation at the current trajectory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="288"/>
+        <source>Assemble</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="291"/>
+        <source>Go to the assembled position of the parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="307"/>
+        <source>Disassemble</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="310"/>
+        <source>Expand all trajectories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="327"/>
+        <source>ExplodeToSelection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="330"/>
+        <source>Expand up to the selected trajectory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="344"/>
+        <source>Error: Select one exploded animation trajectory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="353"/>
+        <source>Trajectory Visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="356"/>
+        <source>Toggle trajectory visibility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="373"/>
+        <source>Align to edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="376"/>
+        <source>Auxiliary tool to align shapes.
+Pick one edge of the object you want to align and
+then the edge of the object used as reference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="404"/>
+        <source>Rotate 15</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="407"/>
+        <source>Auxiliary tool to rotate a shape 15 degrees.
+Select a face of the object you want to rotate
+ and it will be rotated 15 degrees using its normal as rotation
+ axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="427"/>
+        <source>Point to point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="430"/>
+        <source>Auxiliary tool to move point to point.
+Select one point from the shape you want to move 
+ and then the point from other shape where you want to place it</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="452"/>
+        <source>Place concentrically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="455"/>
+        <source>Auxiliary tool to place two shapes concentrically
+Place the first circular edge selected concentric to 
+the second circular edge selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="478"/>
+        <source>Load Example File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="481"/>
+        <source>Load an exploded assembly example file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../InitGui.py" line="43"/>
+        <source>Exploded Assembly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="45"/>
+        <source>Assemble parts and create exploded drawings and animations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="101"/>
+        <source>Exploded Assembly workbench loaded
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ExplodedAssembly_zh-TW.ts
@@ -0,0 +1,292 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW" sourcelanguage="en_US">
+    <extra-po-header-language>zh_TW</extra-po-header-language>
+    <extra-po-header-language_team></extra-po-header-language_team>
+    <extra-po-header-last_translator>DrBenson &lt;Benson.Dr@GMail.com&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date></extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date></extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version>FreeCAD ExplodedAssembly Language Translations v0.2_dev</extra-po-header-project_id_version>
+    <extra-po-header-x_generator>Poedit 3.2.2</extra-po-header-x_generator>
+    <extra-po-header_comment>#
+# Translators:
+# DrBenson &lt;Benson.Dr@GMail.com&gt;, 2023
+#
+    </extra-po-header_comment>
+    <extra-po-headers>Project-Id-Version,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,Language,MIME-Version,Content-Type,Content-Transfer-Encoding,Plural-Forms,X-Language,X-Source-Language,X-Qt-Contexts,X-Generator</extra-po-headers>
+<context>
+    <name>ExplodedAssembly</name>
+    <message>
+        <location filename="../CameraAnimation.py" line="63"/>
+        <source>Enable Camera</source>
+        <translation>啟用相機</translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="64"/>
+        <location filename="../CameraAnimation.py" line="65"/>
+        <source>Interval</source>
+        <translation>間隔</translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="66"/>
+        <location filename="../CameraAnimation.py" line="67"/>
+        <location filename="../CameraAnimation.py" line="68"/>
+        <location filename="../CameraAnimation.py" line="69"/>
+        <source>Camera Position</source>
+        <translation>相機位置</translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="70"/>
+        <source>Camera Transition</source>
+        <translation>相機過渡</translation>
+    </message>
+    <message>
+        <location filename="../CameraAnimation.py" line="100"/>
+        <source>
+Manual camera created
+</source>
+        <translation>
+手動建立相機
+</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="44"/>
+        <source>Create Bolt Group</source>
+        <translation>建立螺栓群組</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="47"/>
+        <source>Create a special exploded group for screws, nuts, bolts... 
+ Select circular edges of the shape you want to animate, then
+ select one face (arbitrary shape) wich has as normal vector
+in the direction in wich you want to move the selected shapes</source>
+        <translation>為螺釘、螺母、螺栓建立一個特殊的爆炸組...
+ 選擇要製作動畫的形狀的圓形邊緣，
+然後在要移動所選形狀的方向上選擇一個面(任意形狀)</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="63"/>
+        <source>Create Simple Group</source>
+        <translation>建立簡單群組</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="66"/>
+        <source>Select the objects you want to explode and
+finally the face which its normal is the trajectory director vector</source>
+        <translation>選擇要分解的對象，最後選擇其法線為軌跡導向向量的面</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="81"/>
+        <source>Create Route</source>
+        <translation>建立旋轉</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="84"/>
+        <source>Select first one or more shapes and last the wire or sketch that represents the trajectory</source>
+        <translation>選擇第一個或多個形狀，最後選擇表示軌跡的導線或草圖</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="101"/>
+        <source>PlaceBefore</source>
+        <translation>擺放前位置</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="104"/>
+        <source>Select the trajectories you want to reallocate (in order) and finally
+the trajectory before which you want to place them</source>
+        <translation>選擇要重新分配的軌跡(按順序)，最後選擇要放置它們的軌跡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="126"/>
+        <source>
+ Select exploded assembly trajectory objects only</source>
+        <translation>
+ 僅選取分解的裝配軌跡物件</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="132"/>
+        <source>Modify Individual Object Trajectory</source>
+        <translation>修改單個物件軌跡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="135"/>
+        <source>Explode objects on several directions at once
+Select the trajectory, select the object and
+the face which its normal is the new director
+vector</source>
+        <translation>一次在多個方向上爆炸物體
+選擇軌跡，選擇其法線
+成為新引導向量的物體和面</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="156"/>
+        <location filename="../EAInit.py" line="174"/>
+        <location filename="../EAInit.py" line="192"/>
+        <source>Manual Animation Camera</source>
+        <translation>手動動畫攝像機</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="159"/>
+        <location filename="../EAInit.py" line="177"/>
+        <location filename="../EAInit.py" line="195"/>
+        <source>Create a transition between two cameras</source>
+        <translation>在兩個攝像機之間建立過渡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="210"/>
+        <source>Play Forward</source>
+        <translation>向前播放</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="213"/>
+        <source>Run the assembly animation</source>
+        <translation>執行零件組合動畫</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="237"/>
+        <source>Play Backwards</source>
+        <translation>向後播放</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="266"/>
+        <source>StopAnimation</source>
+        <translation>停止動畫</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="269"/>
+        <source>Stops the animation at the current trajectory</source>
+        <translation>在目前軌跡處停止動畫</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="288"/>
+        <source>Assemble</source>
+        <translation>組合</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="291"/>
+        <source>Go to the assembled position of the parts</source>
+        <translation>轉到零件的組裝位置</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="307"/>
+        <source>Disassemble</source>
+        <translation>拆卸零件組合</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="310"/>
+        <source>Expand all trajectories</source>
+        <translation>展開所有軌跡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="327"/>
+        <source>ExplodeToSelection</source>
+        <translation>分解到選擇區</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="330"/>
+        <source>Expand up to the selected trajectory</source>
+        <translation>向上展開到所選軌跡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="344"/>
+        <source>Error: Select one exploded animation trajectory</source>
+        <translation>錯誤: 選擇一個分解的動畫軌跡</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="353"/>
+        <source>Trajectory Visibility</source>
+        <translation>軌跡可見</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="356"/>
+        <source>Toggle trajectory visibility</source>
+        <translation>切換軌跡可見性</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="373"/>
+        <source>Align to edge</source>
+        <translation>對齊到邊緣</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="376"/>
+        <source>Auxiliary tool to align shapes.
+Pick one edge of the object you want to align and
+then the edge of the object used as reference</source>
+        <translation>用於對齊形狀的輔助工具.
+選取要對齊的物件的邊緣，
+然後選取用作參照的物件的邊</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="404"/>
+        <source>Rotate 15</source>
+        <translation>旋轉15°</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="407"/>
+        <source>Auxiliary tool to rotate a shape 15 degrees.
+Select a face of the object you want to rotate
+ and it will be rotated 15 degrees using its normal as rotation
+ axis</source>
+        <translation>輔助工具將形狀旋轉 15 度.
+選擇要旋轉的物件的面，
+它將使用其法線作為旋轉軸旋轉 15 度</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="427"/>
+        <source>Point to point</source>
+        <translation>點對點</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="430"/>
+        <source>Auxiliary tool to move point to point.
+Select one point from the shape you want to move 
+ and then the point from other shape where you want to place it</source>
+        <translation>用於點對點移動的輔助工具.
+從要移動的形狀中選擇一個點，
+然後從其他形狀中選擇要放置該點的點</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="452"/>
+        <source>Place concentrically</source>
+        <translation>同心放置</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="455"/>
+        <source>Auxiliary tool to place two shapes concentrically
+Place the first circular edge selected concentric to 
+the second circular edge selected</source>
+        <translation>用於同心軸放置兩個形狀的輔助工具
+將選定的第一條圓邊與選定的第二條圓邊同心放置</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="478"/>
+        <source>Load Example File</source>
+        <translation>載入範例檔案</translation>
+    </message>
+    <message>
+        <location filename="../EAInit.py" line="481"/>
+        <source>Load an exploded assembly example file</source>
+        <translation>載入分解零件組合範例檔案</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../InitGui.py" line="43"/>
+        <source>Exploded Assembly</source>
+        <translation>分解零件組合</translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="45"/>
+        <source>Assemble parts and create exploded drawings and animations</source>
+        <translation>零件組合並建立分解圖紙和動畫</translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="101"/>
+        <source>Exploded Assembly workbench loaded
+</source>
+        <translation>分解零件組合 工作台已載入
+</translation>
+    </message>
+</context>
+</TS>
